85a6fa017b080ac5eda92aa8325308354898f8bb
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2024-09-15 13:21:30 +0300

    Update Program.cs

--- 02. Conditional Statements/02. Exercise/03.TimePlus15Minutes/Program.cs
+++ 02. Conditional Statements/02. Exercise/03.TimePlus15Minutes/Program.cs
@@ -1,19 +1,21 @@
-﻿//Input
+//Input
 int hours = int.Parse(Console.ReadLine());
 int minutes = int.Parse(Console.ReadLine());
 
 //Calculations
-if (minutes >= 45)
+//Minutes
+if (minutes >= 45) //14:47 -> 14:62 -> 15:02
 {
-    hours++;
+    hours++; //++ -> Добавяме 1 към променливата
     minutes = (minutes + 15) % 60;
 }
 else
 {
-    minutes += 15;
+    minutes += 15; //Вместо minutes = minutes + 15; изписваме minutes += 15; -> Така се добавя стойност по-кратко към дадена промнелива
 }
 
-if (hours == 24)
+//Hours
+if (hours == 24) //23:47 -> 0:02
 {
     hours = 0;
 }
