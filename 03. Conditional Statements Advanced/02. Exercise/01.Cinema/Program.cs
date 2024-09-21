b6f0a0072b7856120890f2f19889aaa5e7094921
Nevena Ilieva <118193964+nevenailievaa@users.noreply.github.com>
2024-09-21 15:06:46 +0300

    Add files via upload

---
+++ 03. Conditional Statements Advanced/02. Exercise/01.Cinema/Program.cs
@@ -0,0 +1,23 @@
+﻿//Input
+string projectionType = Console.ReadLine();
+int rowsCount = int.Parse(Console.ReadLine());
+int colsCount = int.Parse(Console.ReadLine());
+
+//Calculations
+double income = 0;
+
+switch (projectionType)
+{
+	case "Premiere":
+		income = rowsCount * colsCount * 12;
+		break;
+    case "Normal":
+        income = rowsCount * colsCount * 7.50;
+        break;
+    case "Discount":
+        income = rowsCount * colsCount * 5;
+        break;
+}
+
+//Output
+Console.WriteLine($"{income:f2} leva");
---
+++ 03. Conditional Statements Advanced/02. Exercise/06.OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,75 @@
+﻿//Input
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+string operation = Console.ReadLine();
+
+//Calculations
+double result = 0;
+bool isEven = false;
+
+if (operation == "+")
+{
+    result = a + b;
+    isEven = result % 2 == 0;
+
+    if (isEven == true)
+    {
+        Console.WriteLine($"{a} + {b} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{a} + {b} = {result} - odd");
+    }
+}
+else if (operation == "-")
+{
+    result = a - b;
+    isEven = result % 2 == 0;
+
+    if (isEven == true)
+    {
+        Console.WriteLine($"{a} - {b} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{a} - {b} = {result} - odd");
+    }
+}
+else if (operation == "*")
+{
+    result = a * b;
+    isEven = result % 2 == 0;
+
+    if (isEven == true)
+    {
+        Console.WriteLine($"{a} * {b} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{a} * {b} = {result} - odd");
+    }
+}
+else if (operation == "/")
+{
+    if (b == 0)
+    {
+        Console.WriteLine($"Cannot divide {a} by zero");
+    }
+    else
+    {
+        result = a / (double)b;
+        Console.WriteLine($"{a} / {b} = {result:f2}");
+    }
+}
+else if (operation == "%")
+{
+    if (b == 0)
+    {
+        Console.WriteLine($"Cannot divide {a} by zero");
+    }
+    else
+    {
+        result = a % b;
+        Console.WriteLine($"{a} % {b} = {result}");
+    }
+}
