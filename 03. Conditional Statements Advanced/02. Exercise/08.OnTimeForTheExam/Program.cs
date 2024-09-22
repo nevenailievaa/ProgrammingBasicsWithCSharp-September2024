6faf8178ec3c4b8570b48a5fadd4a4bd406ed665
Nevena Ilieva <118193964+nevenailievaa@users.noreply.github.com>
2024-09-22 13:26:21 +0300

    Update Program.cs

--- 03. Conditional Statements Advanced/02. Exercise/08.OnTimeForTheExam/Program.cs
+++ 03. Conditional Statements Advanced/02. Exercise/08.OnTimeForTheExam/Program.cs
@@ -1,27 +1,27 @@
-﻿//Input
+//Input
 int examHour = int.Parse(Console.ReadLine());
-int examMinute = int.Parse(Console.ReadLine());
+int examMinutes = int.Parse(Console.ReadLine());
 int studentHour = int.Parse(Console.ReadLine());
-int studentMinute = int.Parse(Console.ReadLine());
-
-int examTimeInMinutes = examHour * 60 + examMinute;
-int studentTimeInMinutes = studentHour * 60 + studentMinute;
+int studentMinutes = int.Parse(Console.ReadLine());
 
 //Calculations
+int examTimeInMinutes = examHour * 60 + examMinutes;
+int studentTimeInMinutes = studentHour * 60 + studentMinutes;
+
 if (examTimeInMinutes < studentTimeInMinutes) //Late
 {
     Console.WriteLine("Late");
     int minutesLate = studentTimeInMinutes - examTimeInMinutes;
 
-	if (minutesLate < 60)
-	{
+    if (minutesLate < 60)
+    {
         Console.WriteLine($"{minutesLate} minutes after the start");
     }
     else
     {
         int hoursLate = minutesLate / 60;
         minutesLate = minutesLate % 60;
-        
+
         if (minutesLate < 10)
         {
             Console.WriteLine($"{hoursLate}:0{minutesLate} hours after the start");
@@ -32,15 +32,15 @@ if (examTimeInMinutes < studentTimeInMinutes) //Late
         }
     }
 }
-else //On Time or Early
+else
 {
     int minutesBefore = examTimeInMinutes - studentTimeInMinutes;
-    
-    if (minutesBefore == 0) //Exactly On Time
+
+    if (minutesBefore == 0) //Exactly on time
     {
         Console.WriteLine("On time");
     }
-    else if (minutesBefore <= 30) //On Time
+    else if (minutesBefore <= 30) //Up to 30 minutes before the start
     {
         Console.WriteLine("On time");
         Console.WriteLine($"{minutesBefore} minutes before the start");
@@ -52,11 +52,11 @@ else //On Time or Early
         int hoursBefore = minutesBefore / 60;
         minutesBefore = minutesBefore % 60;
 
-        if (hoursBefore == 0 && minutesBefore < 60)
+        if (hoursBefore == 0 && minutesBefore < 60) //Early 31 to 59 minutes before the start
         {
             Console.WriteLine($"{minutesBefore} minutes before the start");
         }
-        else if (minutesBefore < 10)
+        else if (minutesBefore < 10) //Early above 1 hour before the start
         {
             Console.WriteLine($"{hoursBefore}:0{minutesBefore} hours before the start");
         }
