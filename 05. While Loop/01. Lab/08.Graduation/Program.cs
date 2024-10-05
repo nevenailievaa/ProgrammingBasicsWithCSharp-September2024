62d4326b5d3a5e319093afbad10c65b2fcb9fb82
Nevena Ilieva <118193964+nevenailievaa@users.noreply.github.com>
2024-10-05 13:36:32 +0300

    Update Program.cs

--- 05. While Loop/01. Lab/08.Graduation/Program.cs
+++ 05. While Loop/01. Lab/08.Graduation/Program.cs
@@ -1,36 +1,26 @@
-﻿//Input
 string studentName = Console.ReadLine();
 
-//Calculations
-double gradesSum = 0;
 int expelsCount = 0;
 int yearsCount = 0;
-
+double gradesSum = 0;
 while (expelsCount < 2 && yearsCount < 12)
 {
     double grade = double.Parse(Console.ReadLine());
     yearsCount++;
-
-    //Expelled
     if (grade < 4)
     {
         expelsCount++;
         continue;
     }
-    //Expelled
-    else
-    {
-        gradesSum += grade;
-    }
+    gradesSum += grade;
 }
 
-//Output
-if (expelsCount == 2)
-{
-    Console.WriteLine($"{studentName} has been excluded at {yearsCount-1} grade");
-}
-else
+if (expelsCount < 2)
 {
     double averageGrade = gradesSum / yearsCount;
     Console.WriteLine($"{studentName} graduated. Average grade: {averageGrade:f2}");
+}
+else
+{
+    Console.WriteLine($"{studentName} has been excluded at {yearsCount-1} grade");
 }
