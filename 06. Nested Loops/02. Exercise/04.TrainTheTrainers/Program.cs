6052047bff930d02a297deab16ffbe0799229438
Nevena Ilieva <118193964+nevenailievaa@users.noreply.github.com>
2024-10-13 13:16:12 +0300

    Update Program.cs

--- 06. Nested Loops/02. Exercise/04.TrainTheTrainers/Program.cs
+++ 06. Nested Loops/02. Exercise/04.TrainTheTrainers/Program.cs
@@ -1,29 +1,30 @@
-﻿//Input
+//Input
 int juryCount = int.Parse(Console.ReadLine());
 
 //Calculations
-string presentationName = "";
 int presentationsCount = 0;
 double allGradesSum = 0;
 double currentGradesSum = 0;
+string presentationName = "";
 
 while ((presentationName = Console.ReadLine()) != "Finish")
 {
     presentationsCount++;
 
-    for (int j = 1; j <= juryCount; j++)
-    {
-        double grade = double.Parse(Console.ReadLine());
-        currentGradesSum += grade;
-        allGradesSum += grade;
-    }
+	for (int i = 1; i <= juryCount; i++)
+	{
+		double grade = double.Parse(Console.ReadLine());
+		currentGradesSum += grade;
+		allGradesSum += grade;
+	}
 
-    double averageGrade = currentGradesSum / juryCount;
-    currentGradesSum = 0;
+	double averageGrade = currentGradesSum / juryCount;
+	currentGradesSum = 0;
 
     //Output
     Console.WriteLine($"{presentationName} - {averageGrade:f2}.");
 }
 
 //Output
-Console.WriteLine($"Student's final assessment is {allGradesSum / (presentationsCount * juryCount):f2}.");
+double averageGradeAll = allGradesSum / (presentationsCount * juryCount);
+Console.WriteLine($"Student's final assessment is {averageGradeAll:f2}.");
