bbfd60cfe4ceb9cce2068d78ea5fb7091f50710e
Nevena Ilieva <118193964+nevenailievaa@users.noreply.github.com>
2024-10-13 13:15:53 +0300

    Update Program.cs

--- 06. Nested Loops/02. Exercise/03.SumPrimeNonPrime/Program.cs
+++ 06. Nested Loops/02. Exercise/03.SumPrimeNonPrime/Program.cs
@@ -1,10 +1,15 @@
-﻿//Input
+// - Ако числото е по-малко или равно на 1, то не е просто.
+// - Ако числото е 2, то е просто (тъй като 2 е най-малкото и единствено четно просто число).
+// - Ако числото е четно и по-голямо от 2, то не е просто.
+// - За нечетни числа, по-големи от 2, проверяваме модулна делимост, започвайки от 3
+// до квадратния корен от числото, като увеличаваме с 2 (за да пропуснем четните числа). Тези числа не са прости.
+
+//Input
 string input = "";
 
 //Calculations
 int primeNumsSum = 0;
 int nonPrimeNumsSum = 0;
-int oddNumsSum = 0;
 
 while ((input = Console.ReadLine()) != "stop")
 {
@@ -17,25 +22,20 @@ while ((input = Console.ReadLine()) != "stop")
         continue;
     }
 
+    //Prime or Non Prime Check
     bool isPrime = true;
 
-    //Prime and Non Prime Check
     if (number <= 1)
     {
         isPrime = false;
     }
-    else if (number == 2)
-    {
-        isPrime = true;
-    }
-    else if (number % 2 == 0)
+    else if (number != 2 && number % 2 == 0)
     {
         isPrime = false;
     }
     else
     {
-        //Check divisibility by odd numbers starting from 3
-        for (int i = 3; i <= Math.Sqrt(number); i += 2)
+        for (int i = 3; i <= Math.Sqrt(number); i+=2)
         {
             if (number % i == 0)
             {
@@ -45,7 +45,6 @@ while ((input = Console.ReadLine()) != "stop")
         }
     }
 
-    //Sum Prime or Non-Prime Numbers
     if (isPrime)
     {
         primeNumsSum += number;
@@ -59,9 +58,3 @@ while ((input = Console.ReadLine()) != "stop")
 //Output
 Console.WriteLine($"Sum of all prime numbers is: {primeNumsSum}");
 Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeNumsSum}");
-
-// - Ако числото е по-малко или равно на 1, то не е просто.
-// - Ако числото е 2, то е просто (тъй като 2 е най-малкото и единствено четно просто число).
-// - Ако числото е четно и по-голямо от 2, то не е просто.
-// - За нечетни числа, по-големи от 2, проверяваме делимост, започвайки от 3
-// до квадратния корен от числото, като увеличаваме с 2 (за да пропуснем четните числа).
