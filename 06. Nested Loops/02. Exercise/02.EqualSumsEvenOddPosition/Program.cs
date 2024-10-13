54e1c8ece9f01cd2e74a9a99ad7690bfdb4c2e8a
Nevena Ilieva <118193964+nevenailievaa@users.noreply.github.com>
2024-10-13 13:15:33 +0300

    Update Program.cs

--- 06. Nested Loops/02. Exercise/02.EqualSumsEvenOddPosition/Program.cs
+++ 06. Nested Loops/02. Exercise/02.EqualSumsEvenOddPosition/Program.cs
@@ -1,20 +1,20 @@
-﻿//Input
+//Input
 int firstNum = int.Parse(Console.ReadLine());
 int secondNum = int.Parse(Console.ReadLine());
 
 //Calculations
-for (int i = firstNum; i <= secondNum; i++)
+for (int currentNum = firstNum; currentNum <= secondNum; currentNum++)
 {
-    string currentNumAsString = i.ToString();
+    string currentNumAsString = currentNum.ToString();
     int evenNumsSum = 0;
     int oddNumsSum = 0;
 
     //Even and Odd Numbers Sum
-    for (int j = 0; j < currentNumAsString.Length; j++)
+    for (int digitPosition = 0; digitPosition < currentNumAsString.Length; digitPosition++)
     {
-        int currentDigit = int.Parse(currentNumAsString[j].ToString());
+        int currentDigit = int.Parse(currentNumAsString[digitPosition].ToString());
 
-        if (j % 2 == 0)
+        if (digitPosition % 2 == 0)
         {
             evenNumsSum += currentDigit;
         }
@@ -27,6 +27,6 @@ for (int i = firstNum; i <= secondNum; i++)
     //Output
     if (evenNumsSum == oddNumsSum)
     {
-        Console.Write(i + " ");
+        Console.Write(currentNum + " ");
     }
 }
